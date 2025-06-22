9245b010e7e1768b70ce1c3ed457ea3d4289161b
VineeThS13 <vineevinee81@gmail.com>
2025-06-22 19:11:21 +0530

    SingletonPatternExample

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Logger
+{
+    private static Logger _instance;
+    private static readonly object _lock = new object();
+
+    private Logger()
+    {
+        Console.WriteLine("Logger initialized.");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+            }
+        }
+        return _instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("Log: " + message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        logger1.Log("This is the first message.");
+
+        Logger logger2 = Logger.GetInstance();
+        logger2.Log("This is the second message.");
+
+        Console.WriteLine($"Are both instances same? {ReferenceEquals(logger1, logger2)}");
+    }
+}
